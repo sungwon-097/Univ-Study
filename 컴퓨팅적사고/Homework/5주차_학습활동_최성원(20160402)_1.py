5941c5c4491742516a084015ad7bbd3bd7704749
sungwon-097 <csw2757@gmail.com>
2022-06-29 20:02:31 +0900

    fixed

--- 컴퓨팅적사고/Homework/5주차_학습활동_최성원(20160402)_1.py
+++ 컴퓨팅적사고/Homework/5주차_학습활동_최성원(20160402)_1.py
@@ -4,10 +4,10 @@
 
 i = 0
 while True:
-    i = int(input("단 수를 입력해 주세요 : "))
+    i = int(input("단 수를 입력해 주세요(자연수 입력) : "))
     if(i > 0):
         break   # i값이 자연수면 루프를 빠져나가 구구단 출력, 그렇지 않을 경우 루프를 돌며 i값을 입력받음
-    print("올바른 범위가 아닙니다(1 이상의 자연수)")
+    print("올바른 범위가 아닙니다(종료)")
 j = 1
 print("====%d단====" %i)
 while j < 10:
@@ -19,10 +19,10 @@ while j < 10:
 l = [1]
 for i in l:
     l.append(i+1) # list의 원소를 추가하며 for 무한루프를 돌도록 함
-    i = int(input("\n단 수를 입력해 주세요(자연수 이외의 수를 입력하면 종료) : "))
+    i = int(input("\n단 수를 입력해 주세요(자연수 입력) : "))
     if i < 1:
-        print("올바른 범위가 아닙니다.")
-        break
+        print("올바른 범위가 아닙니다. 다시 입력해 주세요. ") # i의 값이 0 또는 음수면 실행하지 않음
+        continue
     print("====%d단====" %i)
     for j in range(1, 10):
         print("%d * %d = %2d" %(i, j, i*j))
--- 컴퓨팅적사고/Homework/5주차_학습활동_최성원(20160402)_2_3.py
+++ 컴퓨팅적사고/Homework/5주차_학습활동_최성원(20160402)_2_3.py
@@ -5,8 +5,12 @@
 A = int(input("A를 입력하세요 : "))
 B = int(input("B를 입력하세요 : "))
 
-A_copy = A
-result = 0
+A_copy = A # A값의 보존을 위해 copy
+result = 0 # 결과 값을 저장하는 변수
+
+if A > B:
+    print("A > B, 프로그램 종료")
+    exit(0) # 종료
 while A_copy <= B:
     result += A_copy
     A_copy+=1
@@ -14,9 +18,10 @@ print("%d 부터 %d 까지의 합은 %d 입니다." %(A, B, result))
 
 #   2-2. 교재 228쪽 14번  - 교재문제의 요구상을 만족하도록 작성 (for문 사용)
 
-result = 0
+result = 0 # 결과 값을 담을 변수
+print("\n50부터 100까지의 정수 중에서 3또는 5로 나누어 떨어지는 수 : ", end = "")
 for i in range(50, 101):
     if (i % 3 == 0) | (i % 5 == 0):
         result += i
         print(i, end = " ")
-print("\n50부터 100까지의 정수 중에서 3 또는 5로 나누어 떨어지는 수들의 합 : ", result)
+print("\n합 : ", result)
